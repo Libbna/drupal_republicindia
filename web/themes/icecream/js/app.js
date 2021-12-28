8f36abaae5808ac4f50bfc5abf771b587bfb233e
Libbna <libbna260296@gmail.com>
2021-12-28 15:43:19 +0530

    #3 styled the sliding arrow

--- web/themes/icecream/js/app.js
+++ web/themes/icecream/js/app.js
@@ -1,62 +1,28 @@
-var subMenu = document.getElementById('sub-menu');
-var searchBtn = document.getElementById('search');
-var closeBtn = document.getElementById('close');
-var bars = document.getElementById('bars');
+
+  (function (Drupal) {
+
+    Drupal.behaviors.icecreamBehaviors = {
+      attach: function (context, settings) {
+        var searchBtn = document.getElementById("edit-submit");
+        var inputFrm = document.getElementById('edit-keys');
+        searchBtn.disabled = true;
+
+
+        inputFrm.addEventListener('input', function () {
+          if (inputFrm.value.length >= 2) {
+           searchBtn.disabled = false;
+          } else {
+            searchBtn.disabled = true;
+          }
+        })
+
+      },
+    }
+  })(Drupal);
 
 
 
-// When the user clicks the search btn / hamburger icon, open the modal
-searchBtn.onclick = function () {
-  subMenu.style.display = "block";
-  bars.style.display = "none";
-  closeBtn.style.display = "block";
-  document.body.style.overflow = "hidden";
-}
-bars.onclick = function () {
-  subMenu.style.display = "block";
-  bars.style.display = "none";
-  closeBtn.style.display = "block";
-  document.body.style.overflow = "hidden";
 
-}
 
-// When the user clicks on close button, close the modal
-closeBtn.onclick = function () {
-  subMenu.style.display = "none";
-  bars.style.display = "block";
-  closeBtn.style.display = "none";
-}
-
-  // Drupal Behaviour
-
-  // (function ($, Drupal) {
-  //   Drupal.behaviors.icecreamBehavior = {
-  //     attach: function (context) {
-
-  //       var inputSearch = $(".form-search");
-  //       // var submitBtn = $(".js-form-submit");
-
-  //       $(".js-form-submit").disabled = true;
-
-  //       inputSearch.addEventListener('change', function () {
-  //         if (inputSearch.value.length > 0) {
-  //           submitBtn.disabled = false;
-  //         } else {
-  //           submitBtn.disabled = true;
-  //         }
-  //       });
-  //     }
-  //   }
-  // });
-
-// function ($, Drupal) {
-
-//   // My custom JavaScript code ...
-//   Drupal.behaviors.icecreamBehavior = {
-//     attach: function attach(context) {
-//       $(".js-form-submit").disabled = true;
-//     },
-//   }
-// } (jQuery, Drupal);
 
 
---
+++ web/themes/icecream/js/custom.js
@@ -0,0 +1,28 @@
+var subMenu = document.getElementById('sub-menu');
+var searchBtn = document.getElementById('search');
+var closeBtn = document.getElementById('close');
+var bars = document.getElementById('bars');
+
+// When the user clicks the search btn / hamburger icon, open the modal
+searchBtn.onclick = function () {
+  subMenu.style.display = "block";
+  bars.style.display = "none";
+  closeBtn.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+bars.onclick = function () {
+  subMenu.style.display = "block";
+  bars.style.display = "none";
+  closeBtn.style.display = "block";
+  document.body.style.overflow = "hidden";
+
+}
+
+// When the user clicks on close button, close the modal
+closeBtn.onclick = function () {
+  subMenu.style.display = "none";
+  bars.style.display = "block";
+  closeBtn.style.display = "none";
+}
+
+
