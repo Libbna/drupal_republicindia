f9dd21edc21445c50cbed25cabd3832623e358f7
Libbna <libbna260296@gmail.com>
2022-01-06 14:55:04 +0530

    #14 made header sticky on vertical offset

--- web/themes/icecream/js/custom.js
+++ web/themes/icecream/js/custom.js
@@ -26,3 +26,22 @@ closeBtn.onclick = function () {
 }
 
 
+// sticky header on scroll
+
+window.onscroll = function () {
+  stickyHeader();
+};
+
+var header = document.getElementById('main-header');
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.classList.remove("relative");
+
+  } else {
+    header.classList.remove("sticky");
+    header.classList.add("relative");
+  }
+}
